6d10460419b2043a98be0a554acd656bca4e3c11
Yoshinori Tahara <read.eval.print@gmail.com>
2009-09-06 22:25:08 +0900

    コンパイラを Lisp で書くってことはブートストラップでクロスコンパイルが必要ってことかも。

---
+++ byte-code/compiler/compiler.lisp
@@ -0,0 +1,45 @@
+#|
+(in-package "a")
+(EVAL-WHEN (:COMPILE-TOPLEVEL :LOAD-TOPLEVEL :EXECUTE)
+  (SETQ *PACKAGE* (SB-INT:FIND-UNDELETED-PACKAGE-OR-LOSE "a")))
+とかはどうするんだ？
+.namespace ["a"]
+でいいのか。
+
+トップレベルでいろいろ問題ありそう。
+全体を .sub .end で囲んで、中の defun 等を外出しにすればいいかも。
+|#
+(defun compile-toplevel (form)
+  (if (atom form)
+      (progn
+        "トップレベルでいろいろ問題ありそう。
+全体を .sub .end で囲んで、中の defun 等を外出しにすればいいかも。")
+      (case (car form)
+        (defun
+            (compile-defun (cadr form) (cddr form)))
+        (t
+           "トップレベルでいろいろ問題ありそう。
+全体を .sub .end で囲んで、中の defun 等を外出しにすればいいかも。"
+           ()))))
+
+(defun compile-form (form)
+  (if (atom form)
+      (if (symbolp form)
+          (compile-reference form)
+          (compile-quote form))
+      (case (car form)
+        (defun
+            (compile-defun (cadr form) (cddr form)))
+        (t))))
+
+(defun compile-reference (variable)
+  variable)
+
+(defun compile-quote (value)
+  value)
+
+(defun compile-defun (name body)
+  (format t ".sub '~a'~%" name)
+  (compile-form body)
+  (format t ".end~%"))
+
